2228cea41876856a69c4fb0a75602bed5fa635ce
Apheli0s <71069870+Apheli0s@users.noreply.github.com>
2022-08-17 14:43:21 +0800

    Update Type.h

--- Type.h
+++ Type.h
@@ -1,23 +1,23 @@
 #ifndef SG_TYPE_H
 #define SG_TYPE_H
 
-#include "map"    //map
-#include "memory" //unique_ptr
-#include "array"  //std::array
-#include "vector" //std::vector
+#include "map"     //map
+#include "memory"  //unique_ptr,make_unique()
+#include "array"   //array
+#include "vector"  //vector
+#include "variant" //variant
+#include "string"  //stod()
 
 #include "Json.h"
 #include "JsonValue.h"
 
 using sg_type = enum { SG_NULL,
                        SG_BOOL,
-                       SG_INT,
-                       SG_DOUBLE,
+                       SG_NUMBER,
                        SG_STRING,
                        SG_OBJECT,
                        SG_VECBOOL,
-                       SG_VECINT,
-                       SG_VECDOUBLE,
+                       SG_VECNUMBER,
                        SG_VECSTRING,
                        SG_VECOBJECT
 };
