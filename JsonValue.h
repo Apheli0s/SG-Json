61e44e1232b9cc32b0a802e6235a7ff1eaf8e527
Apheli0s <71069870+Apheli0s@users.noreply.github.com>
2022-08-17 14:27:08 +0800

    Update JsonValue.h

--- JsonValue.h
+++ JsonValue.h
@@ -4,6 +4,8 @@ using namespace std;
 
 namespace sg
 {
+    //曾试图使用union，但如你所见union和非POD的配合是如此错综复杂，于是改用类型安全的C++17的std::variant
+    /*
     union updata //使用union，可以实现同一类拥有不同类数据的指针，同时用智能指针管理对象资源，因为每个unique_ptr大小相同，所以不会产生额外空间。
     {
         unique_ptr<void> pnull;
@@ -17,7 +19,8 @@ namespace sg
         unique_ptr<vector<json>> pvecjson;
 
         updata() : pnull(make_unique<void>()) {}
-        ~updata(); // FIXME:不声明会报错
+        ~updata(); // note: { // FIXME:不声明会报错} Reason:The C++ Standard says (9.5p4):Note : In general, one must use explicit destructor calls
+        // and placement new operators to change the active member of a union.
         updata(bool b) : pbool(make_unique<bool>(b)) {}
         updata(double d) : pnumble(make_unique<double>(d)) {}
         updata(string s) : pstring(make_unique<string>(s)) {}
@@ -27,28 +30,37 @@ namespace sg
         updata(vector<string> vs) : pvecstring(make_unique<vector<string>>(vs)) {}
         updata(vector<json> vj) : pvecjson(make_unique<vector<json>>(vj)) {}
     };
+    */
 
     class json_value
     {
     private:
-        updata u;
+        variant<unique_ptr<void*>,
+                unique_ptr<bool>,
+                unique_ptr<double>,
+                unique_ptr<string>,
+                unique_ptr<json>,
+                unique_ptr<vector<bool>>,
+                unique_ptr<vector<double>>,
+                unique_ptr<vector<string>>,
+                unique_ptr<vector<json>>>
+            v;
         sg_type type;
 
     public:
-        json_value() : type(SG_NULL), u() {} // note:给类的union成员写构造函数以便初始化。
+        json_value() : type(SG_NULL), v(make_unique<void*>()) {} // note:给类的union成员写构造函数以便初始化。
         ~json_value() = default;
 
-        json_value(sg_type &t) : type(t), u() {}
-        json_value(bool &b) : type(SG_BOOL), u(b) {}
-        json_value(double &d) : type(SG_NUMBER), u(d) {}
-        json_value(string &s) : type(SG_STRING), u(s) {}
-        json_value(json &j) : type(SG_OBJECT), u(j) {}
-        json_value(vector<bool> &vb) : type(SG_VECBOOL), u(vb) {}
-        json_value(vector<double> &vd) : type(SG_VECNUMBLE), u(vd) {}
-        json_value(vector<string> &vs) : type(SG_VECSTRING), u(vs) {}
-        json_value(vector<json> &vo) : type(SG_VECOBJECT), u(vo) {}
+        json_value(bool b) : type(SG_BOOL), v(make_unique<bool>(b)) {}
+        json_value(double d) : type(SG_NUMBER), v(make_unique<double>(d)) {}
+        json_value(string s) : type(SG_STRING), v(make_unique<string>(s)) {}
+        json_value(json j) : type(SG_OBJECT), v(make_unique<json>(j)) {}
+        json_value(vector<bool> vb) : type(SG_VECBOOL), v(make_unique<vector<bool>>(vb)) {}
+        json_value(vector<double> vd) : type(SG_VECNUMBER), v(make_unique<vector<double>>(vd)) {}
+        json_value(vector<string> vs) : type(SG_VECSTRING), v(make_unique<vector<string>>(vs)) {}
+        json_value(vector<json> vo) : type(SG_VECOBJECT), v(make_unique<vector<json>>(vo)) {}
 
-        sg_type gettype() { return type; }
-        auto &getu() { return u; };
+        auto gettype() { return type; }
+        auto &getv() { return v; };
     };
 } // namespace sg
