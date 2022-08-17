60b085187b4dd69255b7414bd25fe3657cbd18f7
Apheli0s <71069870+Apheli0s@users.noreply.github.com>
2022-08-17 14:26:51 +0800

    Update Json.h

--- Json.h
+++ Json.h
@@ -12,18 +12,18 @@ namespace sg
     public:
         json(/* args */);
         ~json();
-        json(const string &json_text);
+        json(const string &json_text, int i = 0);
 
-        size_t size() { return members.size() }
+        size_t size() { return members.size(); }
         template <class T>
         void insert(const string &key, T param) { members[key] = make_unique<json_value>(param); }
 
-        inline json_value &operator[](string s) { return *map[s]; }
+        inline json_value &operator[](string s) { return members[s]; }
 
     private:
-        map<string, unique_ptr<json_value>> members;
+        map<string, json_value> members; //以json_value封装数据，并在其内部使用c++17 std::variant保存不同类型数据的unique_ptr,避免需要多个map管理数据。
 
-        void parsevalue(const string &json_text, int i);
+        void parsevalue(const string &key, const string &json_text, int i);
         void parsenull(const string &key, const string &json_text, int i, const char *context);
         void parsebool(const string &key, const string &json_text, int i, const char *context);
         void parsenum(const string &key, const string &json_text, int i);
